0dcdfb0a98160dd55c340bbe19aabbf4f7c8525c
hiwad rashad <hiwadrashad112@hotmail.com>
2021-10-05 16:16:08 +0200

    finished project

--- Youtube_Video_Downloader/mainapp/views.py
+++ Youtube_Video_Downloader/mainapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-import pytube;
+from pytube import YouTube
 
 # Create your views here.
 
 def index(request):
-    url = "https://www.youtube.com/watch?v=t40o7ml-8Ng&list=LL&index=5&ab_channel=101Barz";
-    youtube = pytube.Youtube(url)
-    video = youtube.streams.first()
-    video.download()
-    stringobj = "teststring"
-    return render(request,'mainapp/index.html', {'stringobj':stringobj})
+    if request.method == 'POST' and request.POST:
+     urls = request.POST["youtubeurl"]
+     url = str(urls)
+     youtubeOBJ = YouTube(url)
+     video = youtubeOBJ.streams.first()
+     video.download()
+     return render(request,'mainapp/index.html')
+    else:
+     return render(request,'mainapp/index.html')
+
+
+    
